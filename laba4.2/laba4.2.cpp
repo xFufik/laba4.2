8abcf1ea49aa1baf64926105a479f3ffe78ada66
s8983 <s8983@DESKTOP-HFC69N8>
2023-11-22 15:31:56 +0700

    Добавьте файлы проекта.

---
+++ laba4.2/laba4.2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+#include <locale>
+
+using namespace std;
+
+/*
+*Лабораторная работа 4. Задание 2. Индивидуальный вариант 24.
+*Вычислить примерное значение функции cos в точке X с точностью N с помощью ряда Тейлора.
+*/
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    double x;
+    int n;
+
+    cout << "Введите значение X: ";
+    cin >> x;
+
+    cout << "Введите значение N: ";
+    cin >> n;
+
+    double res = 1;
+    double sum = 1;
+
+    for (int i = 1; i <= n; i++) {
+        sum *= -x * x / (2 * i * (2 * i - 1));
+        res += sum;
+    }
+
+    cout << "Приближенное значение cos(" << x << ") с точностью N = " << n << ": " << res;
+}
